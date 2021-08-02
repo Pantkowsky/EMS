9e661c79edd94b94fe10be744a84c17fa6e74898
Pantkowsky <a.pantkowski@gmail.com>
2021-08-02 22:58:23 +0200

    cleanups

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,33 +1,11 @@
 plugins {
     id(Plugins.APPLICATION)
+    id(Plugins.BUILD)
     id(Plugins.KOTLIN)
 }
 
 android {
     compileSdk = Config.SDK_VERSION
-
-    defaultConfig {
-        applicationId = Config.ID
-        minSdk = Config.SDK_MIN
-        targetSdk = Config.SDK_TARGET
-        versionCode = Config.VERSION_CODE
-        versionName = Config.VERSION_NAME
-        testInstrumentationRunner = Config.TEST_RUNNER
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFile(getDefaultProguardFile("proguard-android-optimize.txt"))
-        }
-    }
-    compileOptions {
-        sourceCompatibility = Config.java
-        targetCompatibility = Config.java
-    }
-    kotlinOptions {
-        jvmTarget = Config.JVM
-    }
 }
 
 dependencies {
