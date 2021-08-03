e2b86c571397c595b32d0b7099237db5f64d1088
Pantkowsky <a.pantkowski@gmail.com>
2021-08-03 21:19:26 +0200

    added `ktlint` to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,19 @@ buildscript {
     }
 }
 
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply {
+        from("${project.rootDir}/linters/ktlint.gradle")
+    }
+}
+
 tasks.apply {
     create<Delete>("clean") {
         delete(rootProject.buildDir)
