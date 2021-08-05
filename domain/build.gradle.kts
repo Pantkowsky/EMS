5afce52c171835db952f58d3a4979fad23ed9ae8
Pantkowsky <a.pantkowski@gmail.com>
2021-08-05 19:16:08 +0200

    added both `rx` and `room` dependencies to the `domain` module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id(Plugins.LIBRARY)
     id(Plugins.BUILD)
+    id(Plugins.KAPT)
     id(Plugins.KOTLIN)
 }
 
 addTestingDependencies()
+addRxDependencies()
+addDatabaseDependencies()
