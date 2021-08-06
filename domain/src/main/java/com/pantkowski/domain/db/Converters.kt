c5d5a5f4293ba54489e600a27df9ab154ad79695
Pantkowsky <a.pantkowski@gmail.com>
2021-08-06 17:46:30 +0200

    added type converter for `address` property of `Employee`

--- domain/src/main/java/com/pantkowski/domain/db/Converters.kt
+++ domain/src/main/java/com/pantkowski/domain/db/Converters.kt
@@ -1,6 +1,9 @@
 package com.pantkowski.domain.db
 
 import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.pantkowski.domain.models.AddressType
 import java.util.UUID
 
 internal class Converters {
@@ -12,4 +15,16 @@ internal class Converters {
     @TypeConverter
     fun fromUUID(uuid: UUID): String =
         uuid.toString()
+
+    @TypeConverter
+    fun toAddress(json: String): Map<AddressType, String> {
+        val addressType = object : TypeToken<Map<AddressType, String>>() {}.type
+        return Gson().fromJson(json, addressType)
+    }
+
+    @TypeConverter
+    fun fromAddress(data: Map<AddressType, String>): String {
+        val gson = Gson()
+        return gson.toJson(data)
+    }
 }
