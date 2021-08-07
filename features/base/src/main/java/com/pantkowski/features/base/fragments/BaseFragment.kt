b2016b4c1fea5af419b38195fe1e4cefe6c6f789
Pantkowsky <a.pantkowski@gmail.com>
2021-08-07 15:23:05 +0200

    added generic `ViewBinding` type for `BaseFragment`

--- features/base/src/main/java/com/pantkowski/features/base/fragments/BaseFragment.kt
+++ features/base/src/main/java/com/pantkowski/features/base/fragments/BaseFragment.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
-abstract class BaseFragment<VB : ViewBinding>(@LayoutRes private val layoutRes: Int) : Fragment() {
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
 
     private var viewBinding: VB? = null
     val binding: VB get() = requireNotNull(viewBinding)
