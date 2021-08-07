31ce84678cefaec329e366a6746c33e4022fb5d0
Pantkowsky <a.pantkowski@gmail.com>
2021-08-07 21:39:40 +0200

    added initial `FeatureNavigator` call to `MainActivity`

--- app/src/main/java/com/pantkowski/ems/MainActivity.kt
+++ app/src/main/java/com/pantkowski/ems/MainActivity.kt
@@ -2,10 +2,14 @@ package com.pantkowski.ems
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pantkowski.features.base.navigation.FeatureNavigator
+import com.pantkowski.features.base.navigation.destinations.Destination
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FeatureNavigator.launch(this, Destination.ROSTER)
+        this.finish()
     }
 }
