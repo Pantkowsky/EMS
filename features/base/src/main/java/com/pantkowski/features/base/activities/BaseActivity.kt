6619591f49bdb75051dbdb320835872535d94a83
Pantkowsky <a.pantkowski@gmail.com>
2021-08-07 21:38:55 +0200

    integrated android `nav` library with base UI components

--- features/base/src/main/java/com/pantkowski/features/base/activities/BaseActivity.kt
+++ features/base/src/main/java/com/pantkowski/features/base/activities/BaseActivity.kt
@@ -1,9 +1,10 @@
 package com.pantkowski.features.base.activities
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
 import org.koin.core.module.Module
@@ -11,14 +12,21 @@ import org.koin.core.module.Module
 abstract class BaseActivity(@LayoutRes private val layoutRes: Int) : AppCompatActivity() {
 
     /**
+     * The id of the [androidx.fragment.app.FragmentContainerView] that acts
+     * as a host for any [androidx.fragment.app.Fragment] to be rendered
+     * withing [BaseActivity]
+     */
+    @get:IdRes abstract val hostId: Int
+
+    /**
      * List of [Module] relevant for the specific child of [BaseActivity]
      * to be used during it's lifecycle
      */
     abstract val modules: List<Module>
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         this.loadDependencies()
-        super.onCreate(savedInstanceState, persistentState)
+        super.onCreate(savedInstanceState)
         setContentView(layoutRes)
     }
 
@@ -35,6 +43,8 @@ abstract class BaseActivity(@LayoutRes private val layoutRes: Int) : AppCompatAc
         unloadDependencies()
         super.onDestroy()
     }
+
+    fun getFeatureNavigator() = findNavController(hostId)
 
     /**
      * On-demand [Module] loading. By default, specific modules
