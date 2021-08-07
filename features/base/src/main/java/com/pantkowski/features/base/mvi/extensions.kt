238efb5b3cb69b7b84cff9317099f2d0e80f9c39
Pantkowsky <a.pantkowski@gmail.com>
2021-08-07 15:22:36 +0200

    added `notOfType` extension function for checking `MviIntent` type

---
+++ features/base/src/main/java/com/pantkowski/features/base/mvi/extensions.kt
@@ -0,0 +1,11 @@
+package com.pantkowski.features.base.mvi
+
+import io.reactivex.rxjava3.annotations.CheckReturnValue
+import io.reactivex.rxjava3.annotations.SchedulerSupport
+import io.reactivex.rxjava3.core.Observable
+
+@CheckReturnValue
+@SchedulerSupport(SchedulerSupport.NONE)
+fun <T : Any, U : Any> Observable<T>.notOfType(clazz: Class<U>): Observable<T> {
+    return filter { !clazz.isInstance(it) }
+}
