8c8b35e79926b5eb08235fea8b9a9658d26809fa
Pantkowsky <a.pantkowski@gmail.com>
2021-08-08 13:16:57 +0200

    added unit tests for `EmployeeMapper`

---
+++ features/roster/src/test/java/com/pantkowski/features/roster/EmployeeMapperTest.kt
@@ -0,0 +1,33 @@
+package com.pantkowski.features.roster
+
+import com.pantkowski.features.roster.internals.models.EmployeeData
+import com.pantkowski.features.roster.internals.usecases.EmployeeMapper
+import com.pantkowski.features.roster.util.adam
+import com.pantkowski.features.roster.util.anna
+import com.pantkowski.features.roster.util.robert
+import org.junit.jupiter.api.Test
+
+class EmployeeMapperTest {
+
+    private val mapper: EmployeeMapper = EmployeeMapper()
+
+    @Test
+    fun `should map EmployeeData correctly`() {
+        val employees = listOf(adam, anna, robert)
+
+        val given = mapper.mapTarget(employees)
+        val expected = EmployeeData(3, employees)
+        val givenValid = given.employees
+            .map { it.isValid() }
+            .reduce { acc, b -> if (b) acc else b }
+        val expectedValid = expected.employees
+            .map { it.isValid() }
+            .reduce { acc, b -> if (b) acc else b }
+
+        assert(expected.count == given.count)
+        assert(expected.employees.size == given.employees.size)
+        assert(expectedValid == givenValid)
+        assert(expected.employees.map { it.age }.reduce(Int::plus)
+            == given.employees.map { it.age }.reduce(Int::plus))
+    }
+}
