ee52866d156d5c375e162a28cc1dcaa7eb9d0561
Pantkowsky <a.pantkowski@gmail.com>
2021-08-08 15:38:31 +0200

    added mapping to `EmployeeModel`

--- features/roster/src/main/java/com/pantkowski/features/roster/internals/usecases/EmployeeMapper.kt
+++ features/roster/src/main/java/com/pantkowski/features/roster/internals/usecases/EmployeeMapper.kt
@@ -3,12 +3,21 @@ package com.pantkowski.features.roster.internals.usecases
 import com.pantkowski.domain.models.Employee
 import com.pantkowski.features.base.usecases.Mapper
 import com.pantkowski.features.roster.internals.models.EmployeeData
+import com.pantkowski.features.roster.internals.models.EmployeeModel
 
 internal class EmployeeMapper : Mapper<List<Employee>, EmployeeData> {
 
     override fun mapTarget(target: List<Employee>): EmployeeData =
         EmployeeData(
             target.size,
-            target
+            target.map { it.toModel() }
+        )
+
+    private fun Employee.toModel(): EmployeeModel =
+        EmployeeModel(
+            this.fullName,
+            this.age,
+            this.gender,
+            this.address
         )
 }
