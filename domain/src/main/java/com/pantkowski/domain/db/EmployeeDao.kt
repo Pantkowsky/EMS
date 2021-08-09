9ce37c1c7869d029e2b9fef50192e6b32604e7dd
Pantkowsky <a.pantkowski@gmail.com>
2021-08-09 19:47:19 +0200

    changed `dao` method definitions

--- domain/src/main/java/com/pantkowski/domain/db/EmployeeDao.kt
+++ domain/src/main/java/com/pantkowski/domain/db/EmployeeDao.kt
@@ -4,10 +4,10 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
 import com.pantkowski.domain.models.Employee
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import java.util.UUID
 
 @Dao
@@ -19,12 +19,15 @@ interface EmployeeDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun add(employee: Employee): Completable
 
-    @Update
-    fun edit(employee: Employee): Completable
+    @Query("UPDATE employees SET salary=:amount WHERE id = :id")
+    fun edit(id: UUID, amount: Long): Completable
 
     @Query("DELETE FROM employees WHERE id = :id")
     fun remove(id: UUID): Completable
 
     @Query("SELECT * FROM employees")
     fun getEmployees(): Observable<List<Employee>>
+
+    @Query("SELECT * FROM employees where id = :id")
+    fun getEmployee(id: UUID) : Single<Employee>
 }
