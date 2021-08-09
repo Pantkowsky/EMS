0da815956cccb708b47a1f2386de1138a8df4ce3
Pantkowsky <a.pantkowski@gmail.com>
2021-08-09 21:16:47 +0200

    renamed `MainActivity` to `SplashActivity`

--- app/src/main/java/com/pantkowski/ems/SplashActivity.kt
+++ app/src/main/java/com/pantkowski/ems/SplashActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.pantkowski.features.base.navigation.FeatureNavigator
 import com.pantkowski.features.base.navigation.destinations.Destination
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
