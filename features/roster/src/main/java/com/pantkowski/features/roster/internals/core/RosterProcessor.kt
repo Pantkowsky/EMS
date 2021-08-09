1c3cf91971a251b705378a75100fffbb7ebf8eef
Pantkowsky <a.pantkowski@gmail.com>
2021-08-09 15:18:28 +0200

    added action processor for `addEmployee`

--- features/roster/src/main/java/com/pantkowski/features/roster/internals/core/RosterProcessor.kt
+++ features/roster/src/main/java/com/pantkowski/features/roster/internals/core/RosterProcessor.kt
@@ -5,7 +5,6 @@ import com.pantkowski.features.roster.internals.models.InitialResult
 import com.pantkowski.features.roster.internals.models.RosterAction
 import com.pantkowski.features.roster.internals.models.RosterResult
 import com.pantkowski.features.roster.internals.usecases.GetEmployeesUseCase
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableTransformer
 
 private typealias Transformer = ObservableTransformer<out RosterAction, RosterResult>
@@ -15,18 +14,30 @@ internal class RosterProcessor(private val useCase: GetEmployeesUseCase) : Actio
 
     override val processors: Map<Transformer, ActionType>
         get() = mapOf(
-            initialProcessor to RosterAction.InitialAction::class.java
+            initialProcessor to RosterAction.InitialAction::class.java,
+            addEmployeeProcessor to RosterAction.AddEmployeeAction::class.java
         )
 
     private val initialProcessor: Transformer
         get() = Transformer { actions ->
-            actions.flatMap { action ->
-                Observable.just(action)
-                    .flatMapSingle { useCase.getEmployeeData() }
+            actions.flatMap {
+                useCase.getEmployeeData()
                     .map { RosterResult.InitialResult.Success(it) }
                     .cast(InitialResult::class.java)
                     .onErrorReturn { RosterResult.InitialResult.Failure(it) }
                     .startWithItem(RosterResult.InitialResult.InFlight)
+            }
+        }
+
+    private val addEmployeeProcessor: Transformer
+        get() = Transformer { actions ->
+            actions.flatMap {
+                useCase.addEmployee()
+                    .andThen(useCase.getEmployeeData())
+                    .map { RosterResult.AddEmployeeResult.Success(it) }
+                    .cast(RosterResult.AddEmployeeResult::class.java)
+                    .onErrorReturn { RosterResult.AddEmployeeResult.Failure(it) }
+                    .startWithItem(RosterResult.AddEmployeeResult.InFlight)
             }
         }
 }
