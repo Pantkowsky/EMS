42cf78a68c24f566c9400a660fe0bb240c5ce40a
Pantkowsky <a.pantkowski@gmail.com>
2021-08-09 15:43:00 +0200

    integrated `EmployeeDiff` for adapter list updates

--- features/roster/src/main/java/com/pantkowski/features/roster/internals/ui/RosterAdapter.kt
+++ features/roster/src/main/java/com/pantkowski/features/roster/internals/ui/RosterAdapter.kt
@@ -3,26 +3,28 @@ package com.pantkowski.features.roster.internals.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.RecyclerView
 import com.pantkowski.features.roster.R
 import com.pantkowski.features.roster.internals.models.EmployeeModel
 
 class RosterAdapter : RecyclerView.Adapter<EmployeeHolder>() {
 
-    private var employees: List<EmployeeModel> = listOf()
+    private val diff: AsyncListDiffer<EmployeeModel> =
+        AsyncListDiffer(this, EmployeeDiff())
 
     override fun getItemCount(): Int =
-        employees.size
+        diff.currentList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeHolder =
         EmployeeHolder(parent.inflate(R.layout.holder_employee))
 
     override fun onBindViewHolder(holder: EmployeeHolder, position: Int) =
-        holder.bind(employees[position])
+        holder.bind(diff.currentList[position])
 
-    fun setEmployees(data: List<EmployeeModel>) {
-        this.employees = data
-        this.notifyDataSetChanged()
+    fun setEmployees(data: List<EmployeeModel>, block: () -> Unit) {
+        diff.submitList(data)
+        block()
     }
 }
 
